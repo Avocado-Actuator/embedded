1239c22b96c7b938655d3ba1d6aaa85e2dcc9640
rystm <rystm@gmail.com>
2018-04-22 22:04:10 -0500

    adding whole project

--- uart_echo.c
+++ uart_echo.c
@@ -34,28 +34,14 @@
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
-#include "driverlib/rom.h"
-#include "driverlib/rom_map.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
+#include "isense.h" // NEW INCLUDE!!!
+#include "utils/uartstdio.h" // NEW INCLUDE!!!
+#include "utils/uartstdio.c" // NEW INCLUDE!!!
+#include "rs485.h" // NEW INCLUDE!!!
 
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>UART Echo (uart_echo)</h1>
-//!
-//! This example application utilizes the UART to echo text.  The first UART
-//! (connected to the USB debug virtual serial port on the evaluation board)
-//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
-//! the UART are transmitted back to the UART.
-//
-//*****************************************************************************
-
-//****************************************************************************
-//
 // System clock rate in Hz.
-//
-//****************************************************************************
 uint32_t g_ui32SysClock;
 
 //*****************************************************************************
@@ -70,163 +56,84 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
+
 //*****************************************************************************
 //
-// Send a string to the UART.
+// Initializes UART0 for console output using UARTStdio
 //
 //*****************************************************************************
 void
-UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+ConsoleInit(void)
 {
-    // Set transceiver rx/tx pin high to send
-    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, GPIO_PIN_6);
+    //
+    // Enable GPIO port A which is used for UART0 pins.
+    // TODO: change this to whichever GPIO port you are using.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
     //
-    // Loop while there are more characters to send.
+    // Configure the pin muxing for UART0 functions on port A0 and A1.
+    // This step is not necessary if your part does not support pin muxing.
+    // TODO: change this to select the port/pin you are using.
     //
-    while(ui32Count--)
-    {
-        //
-        // Write the next character to the UART.
-        //
-        ROM_UARTCharPutNonBlocking(UART7_BASE, *pui8Buffer++);
-    }
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+
+    //
+    // Enable UART0 so that we can configure the clock.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    //
+    // Use the internal 16MHz oscillator as the UART clock source.
+    //
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Select the alternate (UART) function for these pins.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Initialize the UART for console I/O.
+    //
+    UARTStdioConfig(0, 115200, 16000000);
 }
 
 //*****************************************************************************
 //
-// The UART interrupt handler.
-//
-//*****************************************************************************
-void
-UARTIntHandler(void)
-{
-    uint32_t ui32Status;
-
-    //
-    // Get the interrrupt status.
-    //
-    ui32Status = ROM_UARTIntStatus(UART7_BASE, true);
-
-    //
-    // Clear the asserted interrupts.
-    //
-    ROM_UARTIntClear(UART7_BASE, ui32Status);
-
-    // Initialize recv buffer
-    char recv[40] = "";
-    uint32_t ind = 0;
-
-    //
-    // Loop while there are characters in the receive FIFO.
-    //
-    while(ROM_UARTCharsAvail(UART7_BASE))
-    {
-        //
-        // Read the next character from the UART and write it back to the UART.
-        //
-        /*ROM_UARTCharPutNonBlocking(UART7_BASE,
-                                   ROM_UARTCharGetNonBlocking(UART7_BASE));*/
-        recv[ind] = ROM_UARTCharGet(UART7_BASE);
-        ind++;
-
-        //
-        // Blink the LED to show a character transfer is occuring.
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-
-        //
-        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
-        //
-        SysCtlDelay(g_ui32SysClock / (1000 * 3));
-
-        //
-        // Turn off the LED
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
-    }
-
-    // Send back everything that we received in that batch
-    UARTSend((uint8_t *)recv, ind);
-}
-
-//*****************************************************************************
-//
-// This example demonstrates how to send a string of data to the UART.
+// Main function
 //
 //*****************************************************************************
 int
-main(void)
-{
-    //
+main(void) {
     // Set the clocking to run directly from the crystal at 120MHz.
-    //
     g_ui32SysClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
-                                             SYSCTL_OSC_MAIN |
-                                             SYSCTL_USE_PLL |
-                                             SYSCTL_CFG_VCO_480), 120000000);
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+                                                 SYSCTL_OSC_MAIN |
+                                                 SYSCTL_USE_PLL |
+                                                 SYSCTL_CFG_VCO_480), 120000000);
+    ROM_IntMasterEnable(); // Enable processor interrupts.
+    ConsoleInit(); // Initialized UART0 for console output using UARTStdio
+    RSInit(g_ui32SysClock); // Initialize the RS485 link
 
-    //
-    // Enable the GPIO pins for the LED (PN0).
-    //
-    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+    UARTSend((uint8_t *)"\033[2JTiva has turned on\n\r", 24);
+    UARTprintf("Tiva has turned on...\n");
 
-    //
-    // Enable the peripherals used by this example.
-    //
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART7);
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    CurrentSenseInit();
 
-    // Enable GPIO port C pin 6 as the RS-485 transceiver rx/tx pin
-    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_6);
-    // enable tied pin as input to read output of enable pin
-    ROM_GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, GPIO_PIN_7);
-
-    // Write transceiver enable pin low for listening
-    GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0);
-    //
-    // Enable processor interrupts.
-    //
-    ROM_IntMasterEnable();
-
-    //
-    // Set GPIO A0 and A1 as UART pins.
-    //
-    GPIOPinConfigure(GPIO_PC4_U7RX);
-    GPIOPinConfigure(GPIO_PC5_U7TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
-
-    //
-    // Configure the UART for 115,200, 8-N-1 operation.
-    //
-    ROM_UARTConfigSetExpClk(UART7_BASE, g_ui32SysClock, 9600,
-                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-                             UART_CONFIG_PAR_NONE));
-
-    //
-    // Enable the UART interrupt.
-    //
-    ROM_IntEnable(INT_UART7);
-    ROM_UARTIntEnable(UART7_BASE, UART_INT_RX | UART_INT_RT);
-
-    //
-    // Prompt for text to be entered.
-    //
-    UARTSend((uint8_t *)"\033[2JEnter text: ", 16);
-
-    //
     // Loop forever echoing data through the UART.
-    //
+    int timer = 0;
     while(1)
     {
         // Check the busy flag in the uart7 register. If not busy, set transceiver pin low
         if (GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_7) && !UARTBusy(UART7_BASE)){
-            // Set transceiver rx/tx pin low
-            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0);
+            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0); // Set transceiver rx/tx pin low
         }
+        if (timer > 100000){
+            //int ret = getCurrent();
+            timer = 0;
+        }
+        timer++;
     }
 }
