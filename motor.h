0b0a7b56a1b150f372e14c2367df06b796c689b4
rystm <rystm@gmail.com>
2018-04-27 13:32:38 -0500

    init motor files

---
+++ motor.h
@@ -0,0 +1,21 @@
+/*
+ * isense.h
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/fpu.h"
+#include "driverlib/timer.h"
+
+#endif /* ISENSE_H_ */
