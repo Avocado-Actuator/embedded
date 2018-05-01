edc6c2ee420a6ad812b954d091142aed8ca3bdf3
rystm <rystm@gmail.com>
2018-04-30 23:46:28 -0500

    fixed current sensing math

--- isense.h
+++ isense.h
@@ -25,8 +25,9 @@
 // This array is used for storing the data read from the ADC FIFO. It
 // must be as large as the FIFO for the sequencer in use.
 uint32_t isensereadings[4];
+float Current, PrevCurrent, TargetCurrent;
 
 void CurrentSenseInit(void);
-void getCurrent(uint32_t*);
+void getCurrent();
 
 #endif /* ISENSE_H_ */
