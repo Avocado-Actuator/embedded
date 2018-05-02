0801d9616f43e8e3f3fbffa0183652fd7742cbf9
Tsong1030 <yucongchen2017@u.northwestern.edu>
2018-05-02 09:27:35 -0500

    Demo day updates

--- motor.c
+++ motor.c
@@ -10,13 +10,13 @@ void Timer0IntHandler(void)
     ROM_TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
     // Update the interrupt status.
     Time++;
-    if (Time%200==0){
+    if (Time%2000==0){
             time_flag_200ms=1;
         }
-    if (Time%1000==0){
+    if (Time%10000==0){
         time_flag_1000ms=1;
     }
-    if (Time%2==0){
+    if (Time%2==0){//actually 200ns
         time_flag_2ms=1;
     }
 }
@@ -28,19 +28,26 @@ void MotorInit(uint32_t g_ui32SysClock)
     time_flag_200ms = 0;
     time_flag_1000ms = 0;
     time_flag_2ms = 0;
-    frequency = 0; //2500-9000
-    duty=25;
+    frequency = 5000; //2500-9000
+    duty=0;
     KP_velocity=50;
     KI_velocity=90;
     KD_velocity=0;
 
+    KP_angle=0.07;
+    KI_angle=0;
+    KD_angle=0;
+
+    flag=0;
+    CurrentAngle=0;
+    currentAngleError=0;
     /************** Initialization for timer (1ms)  *****************/
     //Enable the timer peripherals
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
     // Configure 32-bit periodic timers.
     //1ms timer
     ROM_TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-    ROM_TimerLoadSet(TIMER0_BASE, TIMER_A, g_ui32SysClock/1000);
+    ROM_TimerLoadSet(TIMER0_BASE, TIMER_A, g_ui32SysClock/10000);//was 1000, trigger every 1ms, 1000Hz
     // Setup the interrupts for the timer timeouts.
     ROM_IntEnable(INT_TIMER0A);
     ROM_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
@@ -65,16 +72,18 @@ void MotorInit(uint32_t g_ui32SysClock)
     PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, 20);
     PWMOutputState(PWM0_BASE, PWM_OUT_4_BIT, true);
     PWMGenEnable(PWM0_BASE, PWM_GEN_2);
+    UARTprintf("initializing motor driver pins\n");
     //Set PQ1 Always high for enable
     GPIOPinTypeGPIOOutput(GPIO_PORTQ_BASE, GPIO_PIN_1);
     GPIOPinWrite(GPIO_PORTQ_BASE, GPIO_PIN_1,GPIO_PIN_1);
 
-    //Set PP0 and PP1 high as nBRAKE and DIR, respectively
+    //Set PP0 and PP1 high as DIR and nBreak, respectively
+    //counterclockwise if PP0 is high
+    //break if PP1 is low
     GPIOPinTypeGPIOOutput(GPIO_PORTP_BASE, GPIO_PIN_0);
     GPIOPinWrite(GPIO_PORTP_BASE, GPIO_PIN_0,GPIO_PIN_0);
     GPIOPinTypeGPIOOutput(GPIO_PORTP_BASE, GPIO_PIN_1);
     GPIOPinWrite(GPIO_PORTP_BASE, GPIO_PIN_1,GPIO_PIN_1);
-    UARTprintf("Motor driver initialized\n");
 }
 
 //Incremental PID control of velocity
@@ -82,46 +91,65 @@ void VelocityControl()
 {
     currentVeloError=TargetVelocity-CurrentVelocity;
 
-    UARTprintf("PWM output:%d.  ",frequency);
-    UARTprintf("Target(count per 0.2sec):%d.  ",TargetVelocity);
-    UARTprintf("speed(count per 0.2sec):%d.  ",CurrentVelocity);
-    UARTprintf("KI:%d. ",currentVeloError);
-    UARTprintf("KP:%d. ",currentVeloError-lastVeloError);
-    UARTprintf("KD:%d. \n",currentVeloError+prevVeloError-2*lastVeloError);
 
-    frequency+=KP_velocity*(currentVeloError-lastVeloError) + KI_velocity*currentVeloError + KD_velocity*(currentVeloError+prevVeloError-2*lastVeloError);//P*(e(k)-e(k-1))+I*e(k)+D*((e(k)-e(k-1))-(e(k-1)-e(k-2)))
+
+    duty+=KP_velocity*(currentVeloError-lastVeloError) + KI_velocity*currentVeloError + KD_velocity*(currentVeloError+prevVeloError-2*lastVeloError);//P*(e(k)-e(k-1))+I*e(k)+D*((e(k)-e(k-1))-(e(k-1)-e(k-2)))
     prevVeloError=lastVeloError;
     lastVeloError=currentVeloError;
 
     //regulator
-    if (frequency<=0)
-        frequency=0;
-    if (frequency>=10000)
-        frequency=10000;
+    if (duty<=0)
+        duty=0;
+    if (duty>=90)
+        duty=90;
 
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, 1875000/frequency);
+    //PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, 1875000/frequency);
     PWMPulseWidthSet(PWM0_BASE, PWM_GEN_2, 1875000/frequency*duty/100);
 }
 
-//Incremental PID control of velocity
+//PD control of position
 void PositionControl()
 {
     currentAngleError=TargetAngle-CurrentAngle;
+    if(currentAngleError<0){
+        currentAngleError+=360;
+    }
     angleErrorInt+=currentAngleError;
     angleErrorDiff=currentAngleError-lastAngleError;
-    frequency+=KP_angle*currentAngleError + KI_angle*angleErrorInt + KD_angle*angleErrorDiff;//P*e(k)+I*sigma(e)+D*(e(k)-e(k-1))
+    duty=KP_angle*currentAngleError + KI_angle*angleErrorInt + KD_angle*angleErrorDiff;//P*e(k)+I*sigma(e)+D*(e(k)-e(k-1))
     lastAngleError=currentAngleError;
 
-    //regulator
-    if (frequency<=0)
-        frequency=0;
-    if (frequency>=10000)
-        frequency=10000;
 
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, 1875000/frequency);
+
+    //if reach the target position, trigger break
+    if(currentAngleError<=5){
+        brake();
+    }
+    //regulator
+    if (duty<=0)
+        duty=0;
+    if (duty>=20)
+        duty=20;
+
+    //PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, 1875000/frequency);
     PWMPulseWidthSet(PWM0_BASE, PWM_GEN_2, 1875000/frequency*duty/100);
 }
 
+//constant speed
+void PositionControlCS()
+{
+    currentAngleError=TargetAngle-CurrentAngle;
+    if(currentAngleError<0){
+        currentAngle+=360;
+    }
+    if(currentAngleError<=30){
+        brake();
+    }
+    else{
+        GPIOPinWrite(GPIO_PORTP_BASE, GPIO_PIN_1,GPIO_PIN_1);
+        testSpin(5000,8);
+    }
+}
 void GetAngle(){
     uint32_t angle, mag, agc, section;
     //Average data over number of cycles
@@ -133,12 +161,28 @@ void GetAngle(){
     return;
 }
 
+//Unit:degree per second
 void GetVelocity(){
-    CurrentVelocity = (CurrentAngle - PrevAngle)/ 0.002; // assumes measuring velocity every 1ms
+    if (CurrentAngle - PrevAngle>=0){
+        CurrentVelocity = (CurrentAngle - PrevAngle)/ 0.002; // assumes measuring velocity every 2ms
+    }
+    else{
+        CurrentVelocity = (CurrentAngle + 360 - PrevAngle)/0.002;
+    }
     return;
 }
 
 void testSpin(uint32_t freq, uint32_t dut){
     PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, 1875000/freq);
     PWMPulseWidthSet(PWM0_BASE, PWM_GEN_2, 1875000/freq*dut/100);
+}
+
+void brake(){
+    flag=1;
+    testSpin(5000,0);
+    GPIOPinWrite(GPIO_PORTP_BASE, GPIO_PIN_1,0);
+}
+
+void disableDriver(){
+    GPIOPinWrite(GPIO_PORTQ_BASE, GPIO_PIN_1,0);
 }
