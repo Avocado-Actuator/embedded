bfaab11bc6af40145beecc9e86ed8ee936614829
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-06-10 20:18:59 -0500

    Clean up isense

--- isense.c
+++ isense.c
@@ -1,124 +1,145 @@
-/*
- * isense.c
- *
- *  Created on: Apr 20, 2018
- *      Author: Ryan
+/**
+ * Implementation of current sensing.
  */
 
 #include "isense.h"
-#include "motor.h"
 
 float Current, TargetCurrent, PrevCurrent, MaxCurrent;
 
 uint32_t offset[3]={0,0,0};
 
+/**
+ * Initialize current reading.
+ */
 void CurrentSenseInit(void) {
-    // Display the setup on the console.
-    // The ADC0 peripheral must be enabled for use.
+    // display the setup on the console
+    // ADC0 peripheral must be enabled for use
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); // Using pins on port D
-    // Configure ADC clock to use the PLL (which is set at 480MHz) divided by 24 to get 20MHz
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); // using port D pins
+    // configure ADC clock to use PLL (set at 480MHz) divided by 24 to get 20MHz
     // and to sample at the full clock rate
     ADCClockConfigSet(ADC0_BASE, ADC_CLOCK_SRC_PLL | ADC_CLOCK_RATE_FULL, 24);
-    // Select the analog ADC function for these pins.
+    // select the analog ADC function for these pins
     GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_7 | GPIO_PIN_6 | GPIO_PIN_5);
-    // Assign a sequence sampler, trigger source, and interrupt priority
+    // assign a sequence sampler, trigger source, and interrupt priority
     ADCSequenceConfigure(ADC0_BASE, 2, ADC_TRIGGER_PROCESSOR, 0);
-    // Assign steps in the reading process. Three current sensors means three steps
-    ADCSequenceStepConfigure(ADC0_BASE,2,0, ADC_CTL_CH6);
-    ADCSequenceStepConfigure(ADC0_BASE,2,1, ADC_CTL_CH5);
-    ADCSequenceStepConfigure(ADC0_BASE,2,2, ADC_CTL_CH4 | ADC_CTL_IE | ADC_CTL_END);
+    // assign steps in reading process, three current sensors means three steps
+    ADCSequenceStepConfigure(ADC0_BASE, 2, 0, ADC_CTL_CH6);
+    ADCSequenceStepConfigure(ADC0_BASE, 2, 1, ADC_CTL_CH5);
+    ADCSequenceStepConfigure(ADC0_BASE, 2, 2, ADC_CTL_CH4 | ADC_CTL_IE | ADC_CTL_END);
     ADCSequenceEnable(ADC0_BASE, 2);
-    // Clear the interrupt status flag.  This is done to make sure the
-    // interrupt flag is cleared before we sample.
+    // clear interrupt status flag
+    // ensures interrupt flag cleared before sampling
     ADCIntClear(ADC0_BASE, 2);
 
     PWMoutput(0);
     SysCtlDelay(12000000);
-    // Determine offset for each CSA
+
+    // determine offset for each CSA
     int j;
-    for (j=0;j<OFFSET_SAMPLES;j++){
-            ADCProcessorTrigger(ADC0_BASE, 2); // Trigger the ADC conversion.
-            while(!ADCIntStatus(ADC0_BASE, 2, false)){} // Wait for conversion to be completed.
-            ADCIntClear(ADC0_BASE, 2); // Clear the ADC interrupt flag.
-            ADCSequenceDataGet(ADC0_BASE, 2, isensereadings); // Read ADC Value.
-            int i;
-            for (i = 0; i < CURRENT_CHANNELS; i++ ){
-//                UARTprintf("Reading %d: %d\n", i, (int)isensereadings[i]);
-                offset[i]+=isensereadings[i];
-            }
-        }
-    for(j=0;j<3;j++){
-        offset[j]=2048-offset[j]/OFFSET_SAMPLES;
-        UARTprintf("offset %d:%d\n",j,offset[j]);
+    for(j = 0; j < OFFSET_SAMPLES; ++j) {
+        ADCProcessorTrigger(ADC0_BASE, 2); // trigger ADC conversion
+        while(!ADCIntStatus(ADC0_BASE, 2, false)) { } // wait for conversion
+
+        ADCIntClear(ADC0_BASE, 2); // clear ADC interrupt flag
+        ADCSequenceDataGet(ADC0_BASE, 2, isensereadings); // read ADC value
+        int i;
+        for(i = 0; i < CURRENT_CHANNELS; ++i)
+            offset[i] += isensereadings[i];
+    }
+    for(j = 0; j < 3; ++j) {
+        offset[j]= 2048 - offset[j]/OFFSET_SAMPLES;
+        UARTprintf("offset %d:%d\n", j, offset[j]);
     }
 
     UARTprintf("Current sensors initialized\n");
 }
 
-float getCurrent() { return Current; }
-void setCurrent(float newCurrent) { Current = newCurrent; }
-float getTargetCurrent() { return TargetCurrent; }
-void setTargetCurrent(float newCurrent) { TargetCurrent = newCurrent; }
-float getMaxCurrent() { return MaxCurrent; }
-void setMaxCurrent(float newMaxCurrent) { MaxCurrent = newMaxCurrent; }
-
-void updateCurrent() {
-    float volt,sum=0;
+/**
+ * Update our most recent current reading
+ *
+ * Read the three Hall sensors and compute a current reading.
+ */
+void updateCurrent(void) {
+    float volt, sum = 0;
     int j;
-    for (j=0;j<CURRENT_SAMPLES;j++){
-        ADCProcessorTrigger(ADC0_BASE, 2); // Trigger the ADC conversion.
-        while(!ADCIntStatus(ADC0_BASE, 2, false)){} // Wait for conversion to be completed.
-        ADCIntClear(ADC0_BASE, 2); // Clear the ADC interrupt flag.
-        ADCSequenceDataGet(ADC0_BASE, 2, isensereadings); // Read ADC Value.
-        uint32_t h1 = GPIOPinRead(GPIO_PORTP_BASE, GPIO_PIN_5); // Read Hall Sensors
+    for(j = 0; j < CURRENT_SAMPLES; ++j) {
+        ADCProcessorTrigger(ADC0_BASE, 2); // trigger ADC conversion
+        while(!ADCIntStatus(ADC0_BASE, 2, false)) { } // wait for conversion
+
+        ADCIntClear(ADC0_BASE, 2); // clear ADC interrupt flag
+        ADCSequenceDataGet(ADC0_BASE, 2, isensereadings); // read ADC value
+        // read Hall sensors
+        uint32_t h1 = GPIOPinRead(GPIO_PORTP_BASE, GPIO_PIN_5);
         uint32_t h2 = GPIOPinRead(GPIO_PORTP_BASE, GPIO_PIN_3);
         uint32_t h3 = GPIOPinRead(GPIO_PORTP_BASE, GPIO_PIN_2);
 
         int i;
-        for (i = 0; i < CURRENT_CHANNELS; i++ ){
-            //UARTprintf("Raw ADC %d:%d\n",i,isensereadings[i]);
-            volt = (float)(isensereadings[i]+offset[i]) / 4095.0 * 3.3; // turns analog value into voltage
-			//UARTprintf("Voltage %d:%d\n",i,(int)(volt*1000));
-            // i = (Vref / 2 - Vmeasured) / (gain * Rsense)
+        for(i = 0; i < CURRENT_CHANNELS; ++i) {
+            // turn analog value into voltage
+            volt = (float) (isensereadings[i] + offset[i]) / 4095.0 * 3.3;
             isense[i]=(3.3/2 - volt) / (CSA_GAIN * 0.055);
         }
 
-//		UARTprintf("H1:%d\n",(int)h1);
-//		UARTprintf("H2:%d\n",(int)h2);
-//		UARTprintf("H3:%d\n",(int)h3);
-		if(direction==1){
-            if(h1>0 && h2==0){
-                sum+=isense[0];
-            }
-            else if(h2>0 && h3==0){
-                sum+=isense[1];
-            }
-            else if(h3>0 && h1==0){
-                sum+=isense[2];
-            }
-		}
-		else{
-		    if(h1==0 && h2>0){
-                sum+=isense[0];
-            }
-            else if(h2==0 && h3>0){
-                sum+=isense[1];
-            }
-            else if(h3==0 && h1>0){
-                sum+=isense[2];
-            }
+		if(direction == 1) {
+            if(h1 > 0 && h2 == 0) { sum += isense[0]; }
+            else if(h2 > 0 && h3 == 0) { sum += isense[1]; }
+            else if(h3 > 0 && h1 == 0) { sum += isense[2]; }
+		} else {
+		    if(h1 == 0 && h2 > 0){ sum += isense[0]; }
+            else if(h2 == 0 && h3 > 0) { sum += isense[1]; }
+            else if(h3 == 0 && h1 > 0) { sum += isense[2]; }
 		}
     }
 
-//	UARTprintf("Current :%d\n",(int)(sum*1000));
-
     PrevCurrent = getCurrent();
-    setCurrent(1000*sum/CURRENT_SAMPLES);//unit mA
+    // unit mA
+    setCurrent(1000 * sum/CURRENT_SAMPLES);
 
-    if(getCurrent()>MAX_CURRENT){
+    if(getCurrent() > MAX_CURRENT) {
         UARTprintf("Drawing too much current! Freeze!");
         brake();
     }
-    return;
 }
+
+/**
+ * Get the current current reading
+ *
+ * @return the current current in mA
+ */
+float getCurrent() { return Current; }
+
+/**
+ * Set the current value
+ *
+ * @param newCurrent - the new current value (in mA) to update with
+ */
+void setCurrent(float newCurrent) { Current = newCurrent; }
+
+/**
+ * Get the current target current
+ *
+ * @return the current target current in mA
+ */
+float getTargetCurrent() { return TargetCurrent; }
+
+/**
+ * Set the current target current
+ *
+ * @param the new target current in mA
+ */
+void setTargetCurrent(float newCurrent) { TargetCurrent = newCurrent; }
+
+/**
+ * Get the current maximum allowable current
+ *
+ * @return the current maximum allowable current in mA
+ */
+float getMaxCurrent() { return MaxCurrent; }
+
+/**
+ * Sets a new maximum current
+ *
+ * @param newMaxCurrent - the new max current value in mA
+ */
+void setMaxCurrent(float newMaxCurrent) { MaxCurrent = newMaxCurrent; }
--- isense.h
+++ isense.h
@@ -1,15 +1,9 @@
-/*
- * isense.h
- *
- *  Created on: Apr 20, 2018
- *      Author: Ryan
- */
-
 #ifndef ISENSE_H_
 #define ISENSE_H_
 
 #include <stdbool.h>
 #include <stdint.h>
+
 #include "inc/hw_memmap.h"
 #include "driverlib/adc.h"
 #include "driverlib/gpio.h"
@@ -17,27 +11,27 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
+
 #include "comms.h"
+#include "motor.h"
 
 #define CURRENT_CHANNELS 3
 #define OFFSET_SAMPLES 20
 #define CURRENT_SAMPLES 50
 #define CSA_GAIN 20
 #define MAX_CURRENT 2000
-//
-// This array is used for storing the data read from the ADC FIFO. It
-// must be as large as the FIFO for the sequencer in use.
+
+// store data read from ADC FIFO, must be as large as FIFO for sequencer in use
 uint32_t isensereadings[4];
 float isense[3];
-//float current_samples[CURRENT_SAMPLES];
 
 void CurrentSenseInit(void);
+void updateCurrent(void);
 
 float getCurrent(void);
 void setCurrent(float);
 float getTargetCurrent(void);
 void setTargetCurrent(float);
-void updateCurrent(void);
 float getMaxCurrent(void);
 void setMaxCurrent(float);
 
