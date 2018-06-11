d240a708e6a26d589a1b73f6690ab19d42263b0f
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-06-10 20:47:10 -0500

    Clean up mag_encoder

--- mag_encoder.h
+++ mag_encoder.h
@@ -1,15 +1,9 @@
-/*
- * encoder.h
- *
- *  Created on: Mar 29, 2018
- *      Author: allentang1996
- */
-
 #ifndef MAG_ENCODER_H_
 #define MAG_ENCODER_H_
 
 #include <stdbool.h>
 #include <stdint.h>
+
 #include "inc/hw_memmap.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
@@ -17,35 +11,24 @@
 #include "driverlib/sysctl.h"
 #include "utils/uartstdio.h"
 
-//*****************************************************************************
-
 #define NUM_SSI_DATA 3
 
-//*****************************************************************************
-//
 // Number of cycles to average
-//
-//*****************************************************************************
 #define AVG_CYCLES 50
 
-//*****************************************************************************
-//
 // AS5048A Flags
-//
-//*****************************************************************************
 #define CMD_READ    0x4000
 #define CMD_WRITE   0x8000
-//*****************************************************************************
-//
+
 // AS5048A Registers
-//
-//*****************************************************************************
 #define REG_AGC         0x3ffd
 #define REG_MAG         0x3ffe
 #define REG_DATA        0x3fff
 #define REG_CLRERR      0x1
 #define REG_ZEROPOS_HI  0x0016
 #define REG_ZEROPOS_LO  0x0017
+
+uint32_t SSISysClock;
 
 void EncoderInit(uint32_t);
 static uint8_t calcEvenParity(uint32_t value);
@@ -55,6 +38,5 @@ uint32_t readRegister(uint32_t addr);
 void readData(uint32_t* angle, uint32_t* mag, char * agc, char * alarmHi, char * alarmLo);
 void readAverageData(uint32_t* angle, uint32_t* mag, uint32_t* agc);
 void zeroPosition(void);
-uint32_t SSISysClock;
 
 #endif /* MAG_ENCODER_H_ */
